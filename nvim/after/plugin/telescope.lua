03931b251b11eebc1d6d06fbc734a3954f552a38
Mateus Rodolfo <mateus.rodolfo.1998@gmail.com>
2023-02-07 11:42:41 +0100

    telescope and tmux modifications

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -1,10 +1,39 @@
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>fo', builtin.buffers, {})
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+local builtin = require("telescope.builtin")
+vim.keymap.set('n', "<leader>ff", builtin.find_files, {})
+vim.keymap.set('n', "<leader>fg", builtin.live_grep, {})
+vim.keymap.set('n', "<leader>fo", builtin.buffers, {})
+vim.keymap.set('n', "<leader>fh", builtin.help_tags, {})
+
+require('telescope').setup{
+  defaults = {
+    -- Default configuration for telescope goes here:
+    -- config_key = value,
+    mappings = {
+      i = {
+        ["<C-j>"] = "move_selection_next",
+        ["<C-k>"] = "move_selection_previous",
+      },
+    }
+  },
+  pickers = {
+    -- Default configuration for builtin pickers goes here:
+    -- picker_name = {
+    --   picker_config_key = value,
+    --   ...
+    -- }
+    -- Now the picker_config_key will be applied every time you call this
+    -- builtin picker
+  },
+  extensions = {
+    -- Your extension configuration goes here:
+    -- extension_name = {
+    --   extension_config_key = value,
+    -- }
+    -- please take a look at the readme of the extension you want to configure
+  }
+}
 
 -- To get telescope-file-browser loaded and working with telescope,
 -- you need to call load_extension, somewhere after setup function:
 require("telescope").load_extension "file_browser"
-vim.keymap.set('n', '<leader>fb', require "telescope".extensions.file_browser.file_browser, {})
+vim.keymap.set("n", "<leader>fb", require "telescope".extensions.file_browser.file_browser, {})
