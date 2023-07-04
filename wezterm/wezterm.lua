e8d110672bb7e7f4ef0d7c73be6bf4e8f7df30ef
Mateus Rodolfo <mateus.rodolfo.1998@gmail.com>
2023-07-04 15:55:07 +0200

    added wezterm config

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,23 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- This table will hold the configuration.
+local config = {}
+
+-- In newer versions of wezterm, use the config_builder which will
+-- help provide clearer error messages
+if wezterm.config_builder then
+  config = wezterm.config_builder()
+end
+
+-- fonts
+config.font = wezterm.font {
+  family = 'CaskaydiaCove Nerd Font',
+  harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' },
+}
+
+-- tab bar
+config.hide_tab_bar_if_only_one_tab = true
+
+-- and finally, return the configuration to wezterm
+return config
