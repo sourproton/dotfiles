b099846bc51188474da7037a6330c8adca7ef902
Mateus Rodolfo <mateus.rodolfo.1998@gmail.com>
2023-07-12 12:31:57 +0200

    lunarvim

---
+++ lvim/config.lua
@@ -0,0 +1,9 @@
+-- Read the docs: https://www.lunarvim.org/docs/configuration
+-- Video Tutorials: https://www.youtube.com/watch?v=sFA9kX-Ud_c&list=PLhoH5vyxr6QqGu0i7tt_XoVK9v-KvZ3m6
+-- Forum: https://www.reddit.com/r/lunarvim/
+-- Discord: https://discord.com/invite/Xb9B4Ny
+
+vim.opt.textwidth = 100
+vim.opt.cc = "+1"
+vim.opt.shiftwidth = 4 -- Size of an indent
+vim.opt.tabstop = 4 -- Number of spaces tabs count for
