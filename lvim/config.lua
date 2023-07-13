2f6276e2210e721787ac2f4aaab23e87605c0675
Mateus Rodolfo <mateus.rodolfo.1998@gmail.com>
2023-07-13 02:16:45 +0200

    send cell keybinds

--- lvim/config.lua
+++ lvim/config.lua
@@ -18,7 +18,7 @@ local function config_tmuxtarget()
     vim.b.tmuxtarget = vim.fn.input("Tmux target: ")
 end
 
--- checks if buffer-variable `tmuxtarget` exists and sends selected text to target
+-- sends selected text to target
 local function send_selection()
     -- checks if `tmuxtarget` exists, calls `config_tmuxtarget` if not
     if vim.b.tmuxtarget == nil then config_tmuxtarget() end
@@ -31,12 +31,29 @@ local function send_selection()
     vim.api.nvim_feedkeys(":silent !tmux send-keys -t " .. vim.b.tmuxtarget .. " Enter\r", "t", true)
 end
 
+-- send cell to target
+local function send_cell()
+    -- checks if `tmuxtarget` exists, calls `config_tmuxtarget` if not
+    if vim.b.tmuxtarget == nil then config_tmuxtarget() end
+
+    -- select current cell
+    vim.api.nvim_feedkeys("/# %%\rNvnk", "t", true)
+
+    -- send selection
+    send_selection()
+
+    -- go to next cell
+    vim.api.nvim_feedkeys("n", "t", true)
+end
+
 lvim.builtin.which_key.mappings["r"] = {
     name = "Tmux send",
     c = { config_tmuxtarget, "Configure target" },
+    s = { send_cell, "Send cell" },
 }
 
 lvim.builtin.which_key.vmappings["r"] = {
     name = "Tmux send",
+    c = { config_tmuxtarget, "Configure target" },
     s = { send_selection, "Send selection" },
 }
