c2f1ec6d21f4697b65b44efae8a1d4a7229e6421
Mateus Rodolfo <mateus.rodolfo.1998@gmail.com>
2023-07-17 22:42:59 +0200

    tunnell config

--- lvim/config.lua
+++ lvim/config.lua
@@ -5,30 +5,17 @@
 
 vim.opt.relativenumber = true
 vim.opt.textwidth = 100
-vim.opt.cc = "+1"                  -- draw vertical line
-vim.opt.shiftwidth = 4             -- Size of an indent
-vim.opt.tabstop = 4                -- Number of spaces tabs count for
+vim.opt.cc = "+1"      -- draw vertical line
+vim.opt.shiftwidth = 4 -- Size of an indent
+vim.opt.tabstop = 4    -- Number of spaces tabs count for
 
 lvim.transparent_window = true
 
 lvim.plugins = {
     {
         "sourproton/tunnell.nvim",
-        config = function()
-            local tunnell = require("tunnell")
-
-            lvim.builtin.which_key.mappings["t"] = {
-                name = "Tunnell",
-                t = { tunnell.send_cell, "Tunnell cell" },
-                p = { tunnell.set_tmux_target, "Set target pane" },
-                h = { tunnell.set_cell_header, "Set cell header" },
-            }
-
-            lvim.builtin.which_key.vmappings["t"] = {
-                name = "Tunnell",
-                t = { tunnell.send_selection, "Tunnell selection" },
-                p = { tunnell.set_tmux_target, "Set target pane" },
-            }
-        end
+        -- dir = "/home/sourproton/tunnell.nvim",
+        -- dev = true,
+        config = true
     }
 }
