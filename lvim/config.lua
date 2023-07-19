bfccdd5ae549c914dd165fa6723b451100b40047
Mateus Rodolfo <mateus.rodolfo.1998@gmail.com>
2023-07-19 15:42:31 +0200

    updated tunnell

--- lvim/config.lua
+++ lvim/config.lua
@@ -16,6 +16,21 @@ lvim.plugins = {
         "sourproton/tunnell.nvim",
         -- dir = "/home/sourproton/tunnell.nvim",
         -- dev = true,
-        config = true
+
+        keys = {
+            { "<leader>tt", ":TunnellCell<CR>",   mode = { "n" }, desc = "Tunnell cell" },
+            { "<leader>tt", ":TunnellRange<CR>",  mode = { "v" }, desc = "Tunnell range" },
+            { "<leader>tc", ":TunnellConfig<CR>", mode = { "n" }, desc = "Tunnell config" },
+        },
+
+        cmd = {
+            "TunnellCell",
+            "TunnellRange",
+            "TunnellConfig",
+        },
+
+        config = function()
+            require("tunnell").setup()
+        end,
     }
 }
