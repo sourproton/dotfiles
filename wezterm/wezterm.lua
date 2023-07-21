049c5b44e2956e4a01c86895b3146a11ce010f16
Mateus Rodolfo <mateus.rodolfo.1998@gmail.com>
2023-07-21 13:15:55 +0200

    wezterm toggle opacity

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,5 +1,5 @@
 -- Pull in the wezterm API
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
 -- This table will hold the configuration.
 local config = {}
@@ -7,13 +7,13 @@ local config = {}
 -- In newer versions of wezterm, use the config_builder which will
 -- help provide clearer error messages
 if wezterm.config_builder then
-  config = wezterm.config_builder()
+    config = wezterm.config_builder()
 end
 
 -- fonts
 config.font = wezterm.font {
-  family = 'CaskaydiaCove Nerd Font',
-  harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' },
+    family = "CaskaydiaCove Nerd Font",
+    harfbuzz_features = { "calt=0", "clig=0", "liga=0" },
 }
 
 -- hide tab bar if only one tab
@@ -22,8 +22,30 @@ config.hide_tab_bar_if_only_one_tab = true
 -- remove title bar
 -- config.window_decorations = "NONE"
 
--- background
-config.window_background_opacity = 0.8
+-- background opacity
+local opacity = 0.8
+config.window_background_opacity = opacity
+
+-- toggle opacity
+wezterm.on("toggle-opacity", function(window, pane)
+    local overrides = window:get_config_overrides() or {}
+    if not overrides.window_background_opacity then
+        -- if no override is setup, override the default opacity value with 1.0
+        overrides.window_background_opacity = 1.0
+    else
+        -- if there is an override, make it nil so the opacity goes back to the default
+        overrides.window_background_opacity = nil
+    end
+    window:set_config_overrides(overrides)
+end)
+
+config.keys = {
+    {
+        key = "O",
+        mods = "CTRL",
+        action = wezterm.action.EmitEvent("toggle-opacity"),
+    },
+}
 
 -- and finally, return the configuration to wezterm
 return config
